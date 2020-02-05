299b2cb9a379b66bd91a0e2f8882d12b3ae284ac
Iosif <iosifscrepy@gmail.com>
2020-02-05 00:38:15 +0000

    Updates

---
+++ precache-manifest.d49b357625d7568ff7d97dc3cc36217c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a22128a6e85278a740435f60b11c65",
+    "url": "/persfinder/index.html"
+  },
+  {
+    "revision": "66769d183e2dcb60a275",
+    "url": "/persfinder/static/css/main.6980d021.chunk.css"
+  },
+  {
+    "revision": "2eecf351bc335b6716c2",
+    "url": "/persfinder/static/js/2.11b9fd8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/persfinder/static/js/2.11b9fd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66769d183e2dcb60a275",
+    "url": "/persfinder/static/js/main.1f3fba9c.chunk.js"
+  },
+  {
+    "revision": "430b91bdb3fc8952cb5a",
+    "url": "/persfinder/static/js/runtime-main.13ca34ff.js"
+  }
+]);
